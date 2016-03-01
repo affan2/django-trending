c0f332fdf55db430aefcc12c05f7dfd8d4617a08
Mohammed Affan <affan2@gmail.com>
2016-03-01 16:37:14 +0400

    added limit because we will never need to return 2k+ rows

--- trending/managers.py
+++ trending/managers.py
@@ -8,7 +8,7 @@ from django.contrib.contenttypes.models import ContentType
 
 
 class TrendingManager(models.Manager):
-    
+
     def trending(self, model, days=30, kind=""):
         views = self.filter(
             viewed_content_type=ContentType.objects.get_for_model(model),
@@ -20,8 +20,8 @@ class TrendingManager(models.Manager):
             "kind"
         ).annotate(
             num_views=Sum("count")
-        ).order_by("-num_views")
-        
+        ).order_by("-num_views")[:50]
+
         for d in views:
             try:
                 d["object"] = ContentType.objects.get_for_id(
