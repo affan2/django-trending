0e5d9591d1faf8c42f3ae01113c7fdb5ad72b700
Mohammad Abouchama <mohammadabouchama@gmail.com>
2019-10-14 17:06:59 +0400

    Disable unnecessary actions on the back-end

--- trending/admin.py
+++ trending/admin.py
@@ -3,5 +3,24 @@ from django.contrib import admin
 from .models import DailyViewSummary, ViewLog
 
 
-admin.site.register(DailyViewSummary)
-admin.site.register(ViewLog)
+class DailyViewSummaryAdmin(admin.ModelAdmin):
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def get_readonly_fields(self, request, obj=None):
+        return [f.name for f in self.model._meta.fields]
+
+
+class ViewLogAdmin(admin.ModelAdmin):
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def get_readonly_fields(self, request, obj=None):
+        return [f.name for f in self.model._meta.fields]
+
+
+admin.site.register(DailyViewSummary, DailyViewSummaryAdmin)
+admin.site.register(ViewLog, ViewLogAdmin)
